8e3a9db64a218c76fa1b2b358eea0462558fefb9
Daniel <danlpsarcanjo@gmail.com>
2022-10-08 11:51:08 -0400

    Add fields to User model

--- VirtualGameStore/Models/User.cs
+++ VirtualGameStore/Models/User.cs
@@ -4,5 +4,10 @@ namespace VirtualGameStore.Models
 {
     public class User : IdentityUser
     {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public bool IsEmailMarketingEnabled { get; set; }
+        public Gender? Gender { get; set; }
     }
 }
