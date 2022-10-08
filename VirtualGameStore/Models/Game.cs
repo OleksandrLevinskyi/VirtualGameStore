55c350423f659ac729648adac550648106d508b4
Oleksandr Levinskyi <aleksandrlevinskyi@gmail.com>
2022-10-08 01:03:16 -0400

    create Game model

---
+++ VirtualGameStore/Models/Game.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VirtualGameStore.Models
+{
+    public class Game
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        public bool IsDigital { get; set; }
+
+        [Required]
+        public int Stock { get; set; }
+    }
+}
