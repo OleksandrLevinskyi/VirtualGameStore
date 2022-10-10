57cb4fd513064e2b66b9343c825ded0147a143c9
Oleksandr Levinskyi <aleksandrlevinskyi@gmail.com>
2022-10-10 16:58:36 -0400

    add data attributes to game model (for validation and styling)

--- VirtualGameStore/Models/Game.cs
+++ VirtualGameStore/Models/Game.cs
@@ -8,15 +8,17 @@ namespace VirtualGameStore.Models
         public int Id { get; set; }
 
         [Required]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         [Required]
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "Price must be greater than or equal to 0.")]
         public double Price { get; set; }
 
         [Required]
+        [Display(Name = "Is Digital")]
         public bool IsDigital { get; set; }
 
         [Required]
