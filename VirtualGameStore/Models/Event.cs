e8412323d21f94e52cfd0f39df7be1f978c6cde1
Oleksandr Levinskyi <aleksandrlevinskyi@gmail.com>
2022-10-10 19:55:41 -0400

    add event model

---
+++ VirtualGameStore/Models/Event.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace VirtualGameStore.Models
+{
+    public class Event
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public User Creator { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime DateTime { get; set; }
+    }
+}
--- VirtualGameStore/Models/User.cs
+++ VirtualGameStore/Models/User.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace VirtualGameStore.Models
 {
     public class User : IdentityUser
     {
+
     }
 }
