c52620274f15547bcad426a777e019b42482a458
Oleksandr Levinskyi <aleksandrlevinskyi@gmail.com>
2022-10-11 17:22:18 -0400

    update Role model

--- VirtualGameStore/Models/Registration.cs
+++ VirtualGameStore/Models/Registration.cs
@@ -7,15 +7,14 @@ namespace VirtualGameStore.Models
         [Key]
         public int Id { get; set; }
 
-        public string? UserId { get; set; }
+        public string UserId { get; set; }
 
         public int EventId { get; set; }
 
-        [Required]
         public DateTime DateTimeRegistered { get; set; } = DateTime.Now;
 
-        public User? User { get; set; }
+        public User User { get; set; }
 
-        public Event? Event { get; set; }
+        public Event Event { get; set; }
     }
 }
