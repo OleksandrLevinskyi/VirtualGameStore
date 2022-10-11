a2efacb0b449cd9f838768113fc2b8cb25f9fd44
Oleksandr Levinskyi <aleksandrlevinskyi@gmail.com>
2022-10-10 21:20:03 -0400

    scaffold event pages

---
+++ VirtualGameStore/Pages/Events/Create.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using VirtualGameStore.Data;
+using VirtualGameStore.Models;
+
+namespace VirtualGameStore.Pages.Events
+{
+    public class CreateModel : PageModel
+    {
+        private readonly VirtualGameStore.Data.ApplicationDbContext _context;
+
+        public CreateModel(VirtualGameStore.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["CreatorId"] = new SelectList(_context.Users, "Id", "Id");
+            return Page();
+        }
+
+        [BindProperty]
+        public Event Event { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Events.Add(Event);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ VirtualGameStore/Pages/Events/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using VirtualGameStore.Data;
+using VirtualGameStore.Models;
+
+namespace VirtualGameStore.Pages.Events
+{
+    public class IndexModel : PageModel
+    {
+        private readonly VirtualGameStore.Data.ApplicationDbContext _context;
+
+        public IndexModel(VirtualGameStore.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Event> Event { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Events != null)
+            {
+                Event = await _context.Events
+                .Include(e => e.Creator).ToListAsync();
+            }
+        }
+    }
+}
