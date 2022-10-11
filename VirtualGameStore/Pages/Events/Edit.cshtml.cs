e7cf14cb4c6b95e9c3bee3d571f76db33f5c9738
Oleksandr Levinskyi <aleksandrlevinskyi@gmail.com>
2022-10-10 22:25:45 -0400

    remove creator id from edit event form

--- VirtualGameStore/Pages/Events/Edit.cshtml.cs
+++ VirtualGameStore/Pages/Events/Edit.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -49,6 +50,7 @@ namespace VirtualGameStore.Pages.Events
                 return Page();
             }
 
+            Event.CreatorId = User.FindFirstValue(ClaimTypes.NameIdentifier);
             _context.Attach(Event).State = EntityState.Modified;
 
             try
