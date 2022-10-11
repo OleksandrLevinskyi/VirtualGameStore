785984507e40efd302da6397396a60d940392c0f
Daniel <danlpsarcanjo@gmail.com>
2022-10-10 21:01:11 -0400

    Add billing address field

---
+++ VirtualGameStore/Areas/Identity/Pages/Account/Manage/Addresses.cshtml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VirtualGameStore.Data;
+using VirtualGameStore.Models;
+
+namespace VirtualGameStore.Areas.Identity.Pages.Account.Manage
+{
+    public class AddressesModel : PageModel
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public AddressesModel(ApplicationDbContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            public Address BillingAddress { get; set; }
+            //public Address ShippingAddress { get; set; }
+        }
+
+        private async Task<User?> GetUser()
+        {
+            if (User.Identity == null)
+            {
+                return null;
+            }
+
+            return await _context.Users
+                .Where(u => u.UserName == User.Identity.Name)
+                .Include(u => u.BillingAddress)
+                .Include(u => u.ShippingAddress)
+                .FirstOrDefaultAsync();
+        }
+
+        private async Task LoadAsync(User user)
+        {
+            Input = new InputModel
+            {
+                BillingAddress = user.BillingAddress ?? new Address(),
+                //ShippingAddress = user.ShippingAddress ?? new Address()
+            };
+        }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            var user = await GetUser();
+
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            await LoadAsync(user);
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await GetUser();
+
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                await LoadAsync(user);
+                return Page();
+            }
+
+            user.BillingAddress = Input.BillingAddress;
+            //user.ShippingAddress = Input.ShippingAddress;
+
+            await _userManager.UpdateAsync(user);
+
+            StatusMessage = "Your preferences have been updated";
+            return RedirectToPage();
+        }
+    }
+}
