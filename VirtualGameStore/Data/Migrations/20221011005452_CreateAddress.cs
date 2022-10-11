b1d9657a82a132635c6b5e9555429e9b1e2f7197
Daniel <danlpsarcanjo@gmail.com>
2022-10-10 20:01:44 -0400

    Create Address

---
+++ VirtualGameStore/Data/Migrations/20221011005452_CreateAddress.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirtualGameStore.Data.Migrations
+{
+    public partial class CreateAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BillingAddressId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ShippingAddressId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Address1 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address2 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Province = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PostalCode = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_BillingAddressId",
+                table: "AspNetUsers",
+                column: "BillingAddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_ShippingAddressId",
+                table: "AspNetUsers",
+                column: "ShippingAddressId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Address_BillingAddressId",
+                table: "AspNetUsers",
+                column: "BillingAddressId",
+                principalTable: "Address",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Address_ShippingAddressId",
+                table: "AspNetUsers",
+                column: "ShippingAddressId",
+                principalTable: "Address",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Address_BillingAddressId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Address_ShippingAddressId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "Address");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_BillingAddressId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_ShippingAddressId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "BillingAddressId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ShippingAddressId",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ VirtualGameStore/Models/Address.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VirtualGameStore.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Address1 { get; set; } = string.Empty;
+        [Display(Name = "Address 2")]
+        public string? Address2 { get; set; }
+        public string City { get; set; } = string.Empty;
+        public string Province { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        [Display(Name = "Postal Code")]
+        public string PostalCode { get; set; } = string.Empty;
+    }
+}
