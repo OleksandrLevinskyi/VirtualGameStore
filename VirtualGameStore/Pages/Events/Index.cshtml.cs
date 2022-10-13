554da23be021748a08af73c9633772072c1e8f6d
Oleksandr Levinskyi <aleksandrlevinskyi@gmail.com>
2022-10-13 14:42:39 -0400

    add member view for events

---
+++ VirtualGameStore/Pages/Events/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using VirtualGameStore.Data;
+using VirtualGameStore.Models;
+
+namespace VirtualGameStore.Pages.Events
+{
+    public class IndexModel : PageModel
+    {
+        private readonly VirtualGameStore.Data.ApplicationDbContext _context;
+
+        public IndexModel(VirtualGameStore.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Event> Events { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Events != null)
+            {
+                Events = await _context.Events
+                .Include(e => e.Creator).
+                ToListAsync();
+            }
+        }
+    }
+}
