561a3f8b57c30a2ba7f9f6dad20272aaadd2ad5d
Daniel <danlpsarcanjo@gmail.com>
2022-10-25 18:54:20 -0400

    wip

---
+++ VirtualGameStore/Pages/FriendsAndFamily/Edit.cshtml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VirtualGameStore.Data;
+using VirtualGameStore.Models;
+
+namespace VirtualGameStore.Pages.FriendsAndFamily
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EditModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Friendship Friendship { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null || _context.Friendship == null)
+            {
+                return NotFound();
+            }
+
+            var friendship =  await _context.Friendship.FirstOrDefaultAsync(m => m.FriendId == id);
+            if (friendship == null)
+            {
+                return NotFound();
+            }
+            Friendship = friendship;
+           ViewData["FriendId"] = new SelectList(_context.Users, "Id", "Id");
+           ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Friendship).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FriendshipExists(Friendship.FriendId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool FriendshipExists(string id)
+        {
+          return _context.Friendship.Any(e => e.FriendId == id);
+        }
+    }
+}
