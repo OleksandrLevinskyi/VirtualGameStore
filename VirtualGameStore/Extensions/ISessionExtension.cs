1b034d5d1ab07cd943109e2069ef154570170e93
Daniel <danlpsarcanjo@gmail.com>
2022-10-25 17:02:04 -0400

    Document ISession.PullString

--- VirtualGameStore/Extensions/ISessionExtension.cs
+++ VirtualGameStore/Extensions/ISessionExtension.cs
@@ -2,6 +2,9 @@
 {
     public static class ISessionExtension
     {
+        /// <summary>
+        /// Retrieves and removes a string from the session.
+        /// </summary>
         public static string? PullString(this ISession session, string key)
         {
             var value = session.GetString(key);
