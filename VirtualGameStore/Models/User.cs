8004485ee79593d5b92f2872f76cd03b9e323fea
Daniel <danlpsarcanjo@gmail.com>
2022-10-25 17:42:08 -0400

    Cleanup User

--- VirtualGameStore/Models/User.cs
+++ VirtualGameStore/Models/User.cs
@@ -5,31 +5,30 @@ namespace VirtualGameStore.Models
 {
     public class User : IdentityUser
     {
-        [PersonalData]
         public string? FirstName { get; set; }
-
-        [PersonalData]
         public string? LastName { get; set; }
-
-        [PersonalData]
         public DateTime? BirthDate { get; set; }
-
-        [PersonalData]
         public bool IsEmailMarketingEnabled { get; set; }
+
         public Gender? Gender { get; set; }
+        public Address? BillingAddress { get; set; }
+        public Address? ShippingAddress { get; set; }
+
         public IEnumerable<Platform> FavoritePlatforms { get; set; }
 
         public List<Review>? Reviews { get; set; }
 
         public List<Event>? Events { get; set; }
-
         public List<Registration>? Registrations { get; set; }
+        public IEnumerable<Category> FavoriteCategories { get; set; }
+        public virtual IEnumerable<PaymentOption> PaymentOptions { get; }
 
-        public Address? BillingAddress { get; set; }
-
-        public Address? ShippingAddress { get; set; }
 
         public bool AreAddressesEqual { get => BillingAddress?.Id == ShippingAddress?.Id; }
+        public bool ArePreferencesEmpty
+        {
+            get => !FavoritePlatforms.Any() || !FavoriteCategories.Any();
+        }
 
         public bool IsBirthDateValid()
         {
@@ -39,14 +38,6 @@ namespace VirtualGameStore.Models
             }
 
             return BirthDate < DateTime.Now;
-        }
-        public IEnumerable<Category> FavoriteCategories { get; set; }
-
-        public virtual IEnumerable<PaymentOption> PaymentOptions { get; }
-
-        public bool ArePreferencesEmpty
-        {
-            get => !FavoritePlatforms.Any() || !FavoriteCategories.Any();
         }
 
         public void ResetPreferences()
