c6fbccc848613c041ab719077d73a86ea7122c40
Daniel <danlpsarcanjo@gmail.com>
2022-10-27 00:12:13 -0400

    Add authorization to cart

--- VirtualGameStore/Pages/Cart/Index.cshtml.cs
+++ VirtualGameStore/Pages/Cart/Index.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -13,6 +14,7 @@ using VirtualGameStore.Models;
 
 namespace VirtualGameStore.Pages.Cart
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
         private readonly VirtualGameStore.Data.ApplicationDbContext _context;
