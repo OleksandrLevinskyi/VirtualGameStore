20ee9e560bab92a7c104884b15231ad96d008c16
Oleksandr Levinskyi <aleksandrlevinskyi@gmail.com>
2022-10-26 20:30:56 -0400

    handle post requests to update review status

--- VirtualGameStore/Pages/Reviews/Admin.cshtml.cs
+++ VirtualGameStore/Pages/Reviews/Admin.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using VirtualGameStore.Data;
 using VirtualGameStore.Models;
@@ -19,16 +20,47 @@ namespace VirtualGameStore.Pages.Reviews
             _context = context;
         }
 
-        public IList<Review> Review { get;set; } = default!;
+        public IList<Review> Reviews { get; set; } = default!;
 
         public async Task OnGetAsync()
         {
             if (_context.Review != null)
             {
-                Review = await _context.Review
+                Reviews = await _context.Review
                 .Include(r => r.Author)
-                .Include(r => r.Game).ToListAsync();
+                .Include(r => r.Game)
+                .ToListAsync();
             }
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id, bool? isApproved)
+        {
+            if (id == null || isApproved == null || _context.Review == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Review.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            review.IsApproved = (bool)isApproved;
+
+            _context.Attach(review).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return NotFound();
+            }
+
+            return RedirectToPage("./Admin");
         }
     }
 }
