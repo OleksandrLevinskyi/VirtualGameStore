168c1fd81d2f7c1177af86e6558fa0a64921c4ea
Oleksandr Levinskyi <aleksandrlevinskyi@gmail.com>
2022-10-26 21:21:05 -0400

    restrict viewing all reviews to employees only

--- VirtualGameStore/Pages/Reviews/Admin.cshtml.cs
+++ VirtualGameStore/Pages/Reviews/Admin.cshtml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -11,6 +13,7 @@ using VirtualGameStore.Models;
 
 namespace VirtualGameStore.Pages.Reviews
 {
+    [Authorize(Roles = "Employee")]
     public class AdminModel : PageModel
     {
         private readonly VirtualGameStore.Data.ApplicationDbContext _context;
