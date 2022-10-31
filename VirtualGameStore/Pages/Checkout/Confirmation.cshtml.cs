755145482c0b140ec32a063be218e3d7551ad472
Daniel <danlpsarcanjo@gmail.com>
2022-10-31 15:39:59 -0400

    add Authorization to order confirmation

--- VirtualGameStore/Pages/Checkout/Confirmation.cshtml.cs
+++ VirtualGameStore/Pages/Checkout/Confirmation.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -8,6 +9,7 @@ using VirtualGameStore.Models;
 
 namespace VirtualGameStore.Pages.Checkout
 {
+    [Authorize]
     public class ConfirmationModel : PageModel
     {
         private readonly VirtualGameStore.Data.ApplicationDbContext _context;
