66a2e3c94b8a054df45f4a16ae1b7dfcc71615a0
Oleksandr Levinskyi <aleksandrlevinskyi@gmail.com>
2022-11-09 01:20:58 -0500

    create a wish list page for sharing in public

---
+++ VirtualGameStore/Pages/WishList/Index.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using VirtualGameStore.Data;
+using VirtualGameStore.Models;
+
+namespace VirtualGameStore.Pages.WishList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public User WishListOwner { get; set; }
+        public List<Game> WishList { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(string? id)
+        {
+            User? currUser = await _context.Users.Include(u => u.WishList).FirstOrDefaultAsync(u => u.Id == id);
+
+            if (currUser == null)
+            {
+                return NotFound();
+            }
+
+            WishListOwner = currUser;
+            WishList = currUser.WishList;
+
+            return Page();
+        }
+    }
+}
