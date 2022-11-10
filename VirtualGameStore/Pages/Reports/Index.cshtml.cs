20170a63515cdd61377dcd862055b332cd1105cc
Tim <timpbh@gmail.com>
2022-11-10 02:24:34 -0500

    First report works

--- VirtualGameStore/Pages/Reports/Index.cshtml.cs
+++ VirtualGameStore/Pages/Reports/Index.cshtml.cs
@@ -1,9 +1,13 @@
 ﻿using System.Net;
 using System.Text;
+using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using VirtualGameStore.Data;
+using VirtualGameStore.Models;
 
 namespace VirtualGameStore.Pages.Reports;
 
@@ -23,6 +27,51 @@ public class IndexModel : PageModel
 
     public async Task<ActionResult> OnPostAsync([Bind] string report)
     {
-        return File(Encoding.ASCII.GetBytes("potato"), "text/csv");
+        byte[] bytes = report switch
+        {
+            ReportTypes.GameList => await CreateReportGameList(),
+            _ => Array.Empty<byte>()
+        };
+        return File(bytes, "application/octet-stream", report + ".xlsx");
+    }
+
+    private async Task<byte[]> CreateReportGameList()
+    {
+        // Prepping values. First, we need to get all the games for the report with details included.
+        var games = await _context.Games
+            .Include(g => g.Categories)
+            .Include(g => g.Platforms)
+            .ToListAsync();
+        // These are the columns that the report will have, set up in this weird array
+        // to make it difficult to accidentally misalign the columns with their values.
+        var reportStyler = new ReportStyler<Game>(new ReportStyler<Game>.Column[]
+        {
+            new("ID", XLDataType.Number, g => g.Id, "0"),
+            new("Name", XLDataType.Text, g => g.Name),
+            new("Price", XLDataType.Number, g => g.Price, "$#,##0.00"),
+            new("IsDigital", XLDataType.Boolean, g => g.IsDigital),
+            new("Stock", XLDataType.Number, g => g.Stock, "#,##0"),
+            new("Categories", XLDataType.Text, g => string.Join(", ", g.Categories!.Select(c => c.Name))),
+            new("Platforms", XLDataType.Text, g => string.Join(", ", g.Platforms!.Select(p => p.Name))),
+        });
+
+        // Make the workbook and add the report to it.
+        using var workbook = new XLWorkbook();
+        var worksheet = reportStyler.AddReportAsSheet(workbook, ReportTypes.Prefix + ReportTypes.GameList, games);
+
+        // Turning into a file...
+        using var memoryStream = new MemoryStream();
+        workbook.SaveAs(memoryStream);
+        return memoryStream.ToArray();
+    }
+
+    public struct ReportTypes
+    {
+        public const string Prefix = "Report - ";
+        public const string GameList = "Game List";
+        public const string MemberList = "Member List";
+        public const string WishList = "Wish List";
+        public const string Sales = "Sales";
+        public const string EventList = "Event List";
     }
 }
---
+++ VirtualGameStore/Pages/Reports/ReportStyler.cs
@@ -0,0 +1,73 @@
+﻿using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace VirtualGameStore.Pages.Reports;
+
+public class ReportStyler<TD>
+{
+    public readonly Column[] ColumnDefinitions;
+
+    public ReportStyler(Column[] columns)
+    {
+        ColumnDefinitions = columns;
+    }
+
+    public IXLWorksheet AddReportAsSheet(XLWorkbook workbook, string name, List<TD> data)
+    {
+        var ws = workbook.Worksheets.Add(name);
+        SetXlsHeaders(ws);
+        SetXlsData(ws, data);
+        SetXlsFormatting(ws);
+        return ws;
+    }
+
+    private void SetXlsHeaders(IXLWorksheet ws)
+    {
+        SetRowValues<string>(ws, 1, ColumnDefinitions.Select(c => c.Label).ToList());
+    }
+
+    private void SetXlsData(IXLWorksheet ws, List<TD> dataItems)
+    {
+        int i = 1;
+        foreach (var item in dataItems)
+            SetRowValues<object>(ws, ++i, ColumnDefinitions.Select(c => c.Value(item)).ToList());
+    }
+
+    private void SetXlsFormatting(IXLWorksheet ws)
+    {
+        int len = ColumnDefinitions.Length;
+        for (int i = 0; i < len; i++)
+        {
+            var columnDef = ColumnDefinitions[i];
+            var columnRange = ws.Range(2, i + 1, len, i + 1);
+            columnRange.DataType = columnDef.DataType;
+            if (columnDef.NumberFormat is not null)
+                columnRange.Style.NumberFormat.Format = columnDef.NumberFormat;
+        }
+
+        ws.Columns().AdjustToContents();
+    }
+
+
+    private static void SetRowValues<TR>(IXLWorksheet ws, int row, List<TR> values)
+    {
+        for (var i = 0; i < values.Count; i++)
+            ws.Cell(row, i + 1).Value = values[i];
+    }
+
+    public struct Column
+    {
+        public readonly string Label;
+        public readonly XLDataType DataType;
+        public readonly Func<TD, object> Value;
+        public readonly string? NumberFormat;
+
+        public Column(string label, XLDataType dataType, Func<TD, object> value, string? numberFormat = null)
+        {
+            Label = label;
+            DataType = dataType;
+            Value = value;
+            NumberFormat = numberFormat;
+        }
+    }
+}
