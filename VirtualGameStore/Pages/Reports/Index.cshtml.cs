b9a65ac129a8baeaebb74e4febbcf7f9749648a9
Tim <timpbh@gmail.com>
2022-11-09 23:20:28 -0500

    Basic page setup for reports that allows downloading a sample file

---
+++ VirtualGameStore/Pages/Reports/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using VirtualGameStore.Data;
+
+namespace VirtualGameStore.Pages.Reports;
+
+[Authorize(Roles = "Employee")]
+public class IndexModel : PageModel
+{
+    private readonly ApplicationDbContext _context;
+
+    public IndexModel(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public void OnGet()
+    {
+    }
+
+    public async Task<ActionResult> OnPostAsync([Bind] string report)
+    {
+        return File(Encoding.ASCII.GetBytes("potato"), "text/csv");
+    }
+}
